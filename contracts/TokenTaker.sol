263ac0427bd3db2de319ec0286ea0fc6c8d9fd70
AzmuthSzym <simondziadek@gmail.com>
2022-05-20 02:09:06 +0200

    TokenTaker - testing ERC20 interactions. Inital commit

---
+++ contracts/TokenTaker.sol
@@ -0,0 +1,8 @@
+////SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.0;
+
+/// @title Contract for tests with ERC20 tokens
+/// @author Azmuth
+/// @notice I just want to know how to interact with ERC20 (taking them from user, returning)
+contract TokenInteraction {
+}
